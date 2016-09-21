5f545b73e946928f5b705d9dd7165da5d9842c13
j <j@j.com>
2016-09-21 20:36:23 +1000

    Styles buttons

--- jtstopwatch/index.ios.js
+++ jtstopwatch/index.ios.js
@@ -29,20 +29,20 @@ var StopWatch = React.createClass({
   render: function(){
     return(
     <View style={styles.container}>
-      <View style={[styles.header,this.border('yellow')]}>
-        <View style={[styles.timeWrapper, this.border('red')]}>
-          <Text>
+      <View style={[styles.header]}>
+        <View style={[styles.timeWrapper]}>
+          <Text style={styles.timer}>
            {convert(this.state.timeElapsed)}
           </Text>
         </View>
 
-        <View style={[styles.buttonWrapper,this.border('green')]}>
+        <View style={[styles.buttonWrapper]}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
       </View>
 
-      <View style={[styles.footer,this.border('blue')]}>
+      <View style={[styles.footer]}>
         <Text>
           I am list of laps
         </Text>
@@ -54,7 +54,9 @@ var StopWatch = React.createClass({
       return(
       <TouchableHighlight
         underlayColor="gray"
-        onPress={this.handleStartPress}>
+        onPress={this.handleStartPress}
+        style={[styles.button,styles.startButton]}
+        >
         <Text>
           Start
         </Text>
@@ -63,7 +65,8 @@ var StopWatch = React.createClass({
   },
   lapButton: function(){
       return(
-      <TouchableHighlight>
+      <TouchableHighlight
+        style={styles.button}>
         <Text>
           Lap.
         </Text>
@@ -118,6 +121,20 @@ const styles = StyleSheet.create({
     flexDirection:"row",
     justifyContent:'space-around',
     alignItems:'center'
+  },
+  timer: {
+    fontSize:60
+  },
+  button:{
+    borderWidth:2,
+    height:100,
+    width:100,
+    borderRadius:50,
+    justifyContent:"center",
+    alignItems:'center'
+  },
+  startButton:{
+    borderColor:'#00CC00'
   }
 });
 /*
